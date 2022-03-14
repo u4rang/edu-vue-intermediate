764538ba22ad1b2545a0a19ad2852cf469b05da4
ChaeFather <u4rang@gmail.com>
2022-03-14 18:57:30 +0900

    components-implementation 작성

---
+++ vue-todo/src/components/TodoList.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+      <ul>
+        <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem.item" class="shadow">
+          <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i>
+          <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
+          <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+            <i class="fas fa-trash-alt"></i>
+          </span>
+          
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      todoItems: [],
+    };
+  },
+
+  methods: {
+    removeTodo: function(todoItem, index) {
+      //console.log(todoItem, index);
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    },
+    toggleComplete: function(todoItem) {
+      // console.log(todoItem, index);
+      todoItem.completed = !todoItem.completed;
+
+      // Update LocalStorage
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
+  },
+
+  // 1.View Life Cycle에서 Created 상태
+  created: function() {
+    if(localStorage.length == 0) return;
+    
+    for(var i = 0; i < localStorage.length; ++i){
+      if(!localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+        // console.log(typeof localStorage.getItem(localStorage.key(i)));
+        // console.log(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+      }
+      
+      // console.log(localStorage.key(i));
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color: #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
+</style>
