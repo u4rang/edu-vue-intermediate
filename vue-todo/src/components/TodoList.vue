05b7f905c8a17b880839c5e37350eb8deda363e1
ChaeFather <u4rang@gmail.com>
2022-03-17 10:25:09 +0900

    vuex의 helper 적용

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
     <transition-group name="list" tag="ul">
-        <li v-for="(todoItem, index) in this.$store.state.todoItems" v-bind:key="todoItem.item" class="shadow">
-          <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i>
+        <li v-for="(todoItem, index) in this.storedTodoItems" v-bind:key="todoItem.item" class="shadow">
+          <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete({todoItem, index})"></i>
           <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
-          <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+          <span class="removeBtn" v-on:click="removeTodo({todoItem, index})">
             <i class="fas fa-trash-alt"></i>
           </span>
         </li>
@@ -13,17 +13,27 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+import { mapState } from 'vuex'
+import { mapMutations } from 'vuex'
+
 export default {
   methods: {
-    removeTodo: function(todoItem, index) {
-      //console.log(todoItem, index);
-      this.$store.commit('removeOneItem', {todoItem, index});
-    },
-    toggleComplete: function(todoItem, index) {
-      this.$store.commit('toggleOneItem', {todoItem, index});
-    },
+    ...mapMutations({
+      removeTodo: 'removeOneItem',
+      toggleComplete : 'toggleOneItem'
+    }),
   },
-
+  computed: {
+    // todoItems() {
+    //   return this.$store.getters.storedTodoItems;
+    // }
+    ...mapState(['todoItems']),
+    ...mapGetters(['storedTodoItems']),
+    // ...mapGetters({
+    //   todoItems: 'storedTodoItems'
+    // })
+  }
   
 }
 </script>
